933adb0a233b4543e5a3f2de299eacbaa9ea19a8
Antoniomp5 <antoniohg2001@gmail.com>
2025-03-25 14:15:23 +0100

    Add files via upload

---
+++ AntonioHernandezGilabert_ExUD4-ED/unit-testing-using-nunit/PrimeService.cs
@@ -0,0 +1,13 @@
+﻿namespace unit_testing_using_nunit;
+
+using System;
+namespace Prime.Services
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            throw new NotImplementedException("Please create a test first.");
+        }
+    }
+}
